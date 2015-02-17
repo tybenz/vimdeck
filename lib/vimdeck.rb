076200477773cdc62bbff58c602e068d71e83ab1
Tyler Benziger <tabenziger@gmail.com>
2015-02-17 08:13:35 -0800

    Possible fix for #29 (DOS newlines)- needs to be tested on Windows machine

--- lib/vimdeck.rb
+++ lib/vimdeck.rb
@@ -5,6 +5,8 @@ require 'asciiart'
 require 'erb'
 require 'redcarpet'
 
+$nl = "\n"
+
 module Vimdeck
   # Helper methods for ascii art conversion
   class Ascii
@@ -78,34 +80,34 @@ module Vimdeck
 
     def list(content, type)
       if type == :unordered
-        "<!~#{content}~!>\n\n"
+        "<!~#{content}~!>#{$nl}#{$nl}"
       else
-        "<@~#{content}~@>\n\n"
+        "<@~#{content}~@>#{$nl}#{$nl}"
       end
     end
 
     def header(title, level)
       margin = Vimdeck::Slideshow.options[:header_margin]
-      linebreak = margin ? "\n" * margin : "\n"
+      linebreak = margin ? "#{$nl}" * margin : "#{$nl}"
       if !Vimdeck::Slideshow.options[:no_ascii]
         case level
         when 1
           heading = Vimdeck::Ascii.header(title, "large")
           if Vimdeck::Slideshow.options[:no_indent]
-            heading = "    " + heading.gsub( /\n/, "\n    " ) + linebreak
+            heading = "    " + heading.gsub( /\r\n?|\n/, "#{$nl}    " ) + linebreak
           else
             heading + linebreak
           end
         when 2
           heading = Vimdeck::Ascii.header(title, "small")
           if Vimdeck::Slideshow.options[:no_indent]
-            heading = "    " + heading.gsub( /\n/, "\n    " ) + linebreak
+            heading = "    " + heading.gsub( /\r\n?|\n/, "#{$nl}    " ) + linebreak
           else
             heading + linebreak
           end
         end
       else
-        title + "\n\n"
+        title + "#{$nl}#{$nl}"
       end
     end
 
@@ -114,11 +116,11 @@ module Vimdeck
     end
 
     def paragraph(text)
-      text + "\n\n"
+      text + "#{$nl}#{$nl}"
     end
 
     def block_code(code, language)
-      "```#{language}\n#{code}\n```"
+      "```#{language}#{$nl}#{code}#{$nl}```"
     end
 
     def image(image, title, alt_text)
@@ -145,6 +147,9 @@ module Vimdeck
     def self.generate(filename, options)
       @options = options
       extension = options[:no_filetype] ? ".txt" : ".md"
+      if options[:dos_newlines]
+        $nl = "\r\n"
+      end
       slides = File.read(filename)
 
       renderer = Redcarpet::Markdown.new(Vimdeck::Render, :fenced_code_blocks => true)
@@ -152,7 +157,7 @@ module Vimdeck
       @buffers = []
 
       # Slide separator is 3 newlines
-      slides = slides.split("\n\n\n")
+      slides = slides.split(/(?:\r\n?|\n)(?:\r\n?|\n)(?:\r\n?|\n)/)
       i = 0
       slides.each do |slide|
         # Pad file names with zeros. e.g. slide001.md, slide023.md, etc.
@@ -162,24 +167,24 @@ module Vimdeck
         regex = /\<\@\~(.*?)\~\@\>/m
         match = slide.match(regex)
         while match && match[1] && match.post_match do
-          list = match[1].split("\n")
+          list = match[1].split(/\r\n?|\n/)
           j = 0
           list = list.map do |li|
             j += 1
             "#{j}. #{li}"
           end
-          slide.sub!(regex, list.join("\n"))
+          slide.sub!(regex, list.join($nl))
           match = match.post_match.match(regex)
         end
 
         regex = /\<\!\~(.*?)\~\!\>/m
         match = slide.match(regex)
         while match && match[1] && match.post_match do
-          list = match[1].split("\n")
+          list = match[1].split(/\r\n?|\n/)
           list = list.map do |li|
             "\u2022 #{li}"
           end
-          slide.sub!(regex, list.join("\n"))
+          slide.sub!(regex, list.join($nl))
           match = match.post_match.match(regex)
         end
 
@@ -189,17 +194,17 @@ module Vimdeck
         buffer = {:num => i + 1}
         code_height = 0
         code = nil
-        code = slide.match( /```([^\n]*)\n.*\n```/m )
+        code = slide.match( /```([^\r\n]*)(\r\n?|\n).*(\r\n?|\n)```/m )
         if code
           buffer[:code] = []
           code_hash = { :language => code[1] }
-          code_height = code[0].split("\n").length - 2
-          code = code[0].gsub( /```[^\n]*\n/, '' ).gsub( /\n```/, '' )
-          slide = slide.gsub( /```[^\n]*\n/, '' ).gsub( /\n```/, '' )
+          code_height = code[0].split(/\r\n?|\n/).length - 2
+          code = code[0].gsub( /```[^\r\n]*(\r\n?|\n)/, '' ).gsub( /(\r\n?|\n)```/, '' )
+          slide = slide.gsub( /```[^\r\n]*(\r\n?|\n)/, '' ).gsub( /(\r\n?|\n)```/, '' )
 
           if code_height > 0
             start = slide.index(code)
-            start = slide[0..start].split("\n").length
+            start = slide[0..start].split(/\r\n?|\n/).length
             code_hash[:end] = code_height + start - 1
             code_hash[:start] = start
           end
@@ -209,7 +214,7 @@ module Vimdeck
         # Prepending each line with slide_padding
         # Removing trailing spaces
         # Add newlines at end of the file to hide the slide identifier
-        slide = slide_padding + slide.gsub( /\n/, "\n#{slide_padding}" ).gsub( / *$/, "" ) + ("\n" * 80) + "slide #{slide_num}"
+        slide = slide_padding + slide.gsub( /\r\n?|\n/, "#{$nl}#{slide_padding}" ).gsub( / *$/, "" ) + ($nl * 80) + "slide #{slide_num}"
 
         # Buffers comments refers to items that need to be less focused/"unhighlighted"
         # We add a regex to the vimscript for each slide with "comments"
@@ -220,12 +225,12 @@ module Vimdeck
         while match && match[1] && match.post_match do
           slide.sub!(regex, match[1])
           pattern = match[1] + "||(||_.*slide #{slide_num}||)||@="
-          buffer[:comments] << pattern.gsub(/\n/, "||n").gsub(/\[/, "||[").gsub(/\]/, "||]").gsub(/\|/, "\\").gsub(/\"/, "\\\"")
+          buffer[:comments] << pattern.gsub(/\r\n?|\n/, "||n").gsub(/\[/, "||[").gsub(/\]/, "||]").gsub(/\|/, "\\").gsub(/\"/, "\\\"")
           match = match.post_match.match(regex)
         end
 
         File.open("presentation/slide#{slide_num}#{extension}", "w") do |file|
-          file.write(slide + "\n")
+          file.write("#{slide}#{$nl}")
         end
 
         @buffers << buffer
